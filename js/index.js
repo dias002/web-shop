91c2194cdf9d854f7889ebd9cfba9b37867bdde8
dias002 <95093334+dias002@users.noreply.github.com>
2023-09-11 11:18:01 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const burgerBtn = document.querySelector('.header_burger')
+const burgerContent = document.querySelector('.burger_content')
+
+burgerBtn.addEventListener('click', function(){
+    burgerContent.classList.toggle('moved')
+    this.classList.toggle('burger_active')
+})
+
+const modalBtn = document.querySelectorAll('[data-modal-button')
+const modals = document.querySelectorAll('[data-modal')
+const modalClose = document.querySelectorAll    ('[data-modal-close]')
+
+modalClose.forEach(function(item){
+    item.addEventListener('click', function(){
+        const modalCloseID = this.dataset.modalClose
+        const thisWndw = document.querySelector('#' + modalCloseID)
+        thisWndw.classList.add('hidden')
+    })
+})
+
+modalBtn.forEach (function(item){
+    item.addEventListener('click', function(){
+        const modalId = this.dataset.modalButton
+        const modalWndw = document.querySelector('#' + modalId)
+        modalWndw.classList.remove('hidden')
+        modalWndw.querySelector('.modal_info').addEventListener('click', function(e){
+            e.stopPropagation();
+        })
+
+    })
+});
+modals.forEach (function(item){
+    item.addEventListener('click', function(){
+        this.classList.add('hidden')
+    })
+});
---
+++ process.php
@@ -0,0 +1,27 @@
+<?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    // Получите данные из формы
+                    $fullName = $_POST["fullName"];
+                    $phoneNumber = $_POST["phoneNumber"];
+                    
+                    // Адрес, на который будет отправлено письмо
+                    $to = "dias.sunnatilla@gmail.com";
+                    
+                    // Тема письма
+                    $subject = "Регистрация нового пользователя";
+                    
+                    // Сообщение, которое будет отправлено
+                    $message = "ФИО: " . $fullName . "\n";
+                    $message .= "Number " . $email . "\n";
+                    
+                    // Заголовки для письма
+                    $headers = "From: " . $email . "\r\n";
+                    $headers .= "Reply-To: " . $email . "\r\n";
+                    
+                    // Отправка письма
+                    mail($to, $subject, $message, $headers);
+                    
+                    // Перенаправление на страницу подтверждения
+                    header("Location: confirmation.html");
+                }
+                ?>
